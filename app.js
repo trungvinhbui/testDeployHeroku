7f2349ad3f0fcb3e74453908a36214b7f0c40dab
Bui Phuc Trung Vinh <vinhbpt@vng.com.vn>
2018-12-20 15:17:16 +0700

    json ZaloPay callback

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const http = require('http');
 const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+    res.setHeader('Content-Type', 'application/json');
+    res.end('{"returncode": "1","returnmessage": ""}');
 });
 server.listen(port, () => {
     console.log(`Server running at port ` + port);
